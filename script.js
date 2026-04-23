40b2b1d8d3d6b36e7783fe58691cc437145a4fc0
MaruDang311 <the311dangsculture@gmail.com>
2026-04-23 09:19:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let currentLang = localStorage.getItem("lang") || "en";
+let currentLang = localStorage.getItem("lang") || "gu"; // Default Gujarati
 
 function setLanguage(lang) {
     let elements = document.querySelectorAll("[data-en]");
-
+    
     elements.forEach(el => {
         if (lang === "gu") {
             el.innerText = el.getAttribute("data-gu");
@@ -12,14 +12,27 @@ function setLanguage(lang) {
     });
 
     document.getElementById("langBtn").innerText = (lang === "gu") ? "English" : "ગુજરાતી";
+
     localStorage.setItem("lang", lang);
 }
 
+// Toggle button
 function toggleLanguage() {
-    currentLang = (currentLang === "en") ? "gu" : "en";
+    currentLang = (currentLang === "gu") ? "en" : "gu";
     setLanguage(currentLang);
 }
 
+// Auto detect browser language (first time only)
 window.onload = () => {
+    if (!localStorage.getItem("lang")) {
+        let browserLang = navigator.language;
+
+        // Gujarati priority
+        if (browserLang.startsWith("gu")) {
+            currentLang = "gu";
+        } else {
+            currentLang = "gu"; // fallback પણ Gujarati
+        }
+    }
     setLanguage(currentLang);
 };
